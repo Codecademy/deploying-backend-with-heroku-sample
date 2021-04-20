3bca5e4d0aea73698c44e237d381810cb2c94057
Jake Adler <jakeadler@MacBook-Pro-de-Jessica.local>
2021-04-20 04:38:48 +0400

    Update

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require('express'); // import express module (simplifies routing/requests, among other things)
-const app = express(); // create an instance of the express module (app is the conventional variable name used)
-const fetch = require('node-fetch'); // import node-fetch (enables the fetch API to be used server-side)
-const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
+var Zabo = require('zabo-sdk-js')
 
-app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
-  fetch('https://www.boredapi.com/api/activity') // fetch activity from bored API - https://www.boredapi.com/about
-    .then(res => res.json()) // return a promise containing the response
-    .then(json => res.send(`<h1>Today's Activity: ${json.activity}!</h1>`)) // extract the JSON body content from the response (specifically the activity value) and sends it to the client
-    .catch(function(err){ // catch any errors
-      console.log(err); // log errors to the console
-    })
+abo.init({
+  clientId: 'obvI09LujjmnRcAzZAm8ykS3nycZlPhhYy8tg1uftUXLQgtvlPNpEoWLEuNYDBru',
+  env: 'sandbox'
+}).then(zabo => {
+  // `zabo` is ready to be used 
 })
 
-app.listen(PORT, () => { // start server and listen on specified port
-  console.log(`App is running on ${PORT}`) // confirm server is running and log port to the console
-}) 
+// Here we add a listener to the first button in our html file
+document.querySelector('button').addEventListener('click', () => {
+  // We call the .connect() window when it is clicked, and provide a callback to `onConnection`
+  zabo.connect().onConnection((account) => {
+    console.log(account)
+  }).onError(error => {
+    console.error('account connection error:', error)
+  })
+})
