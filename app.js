e2a3214af68b4faa77bbf7807bbc5bdfa3a5dcd1
douglasdriving <douglasdriving@gmail.com>
2022-10-26 17:07:44 +0200

    added db connection

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express'); // import express module (simplifies routing
 const app = express(); // create an instance of the express module (app is the conventional variable name used)
 const fetch = require('node-fetch'); // import node-fetch (enables the fetch API to be used server-side)
 const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
+const bodyParser = require('body-parser');
+const Pool = require('pg').Pool
+
+//HEROKU DB CONNECTION
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
   fetch('https://www.boredapi.com/api/activity') // fetch activity from bored API - https://www.boredapi.com/about
