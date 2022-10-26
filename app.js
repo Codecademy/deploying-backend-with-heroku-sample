62aac1461b22bdd13eb9568e0f8a38ddcd5ae3de
douglasdriving <douglasdriving@gmail.com>
2022-10-26 17:33:52 +0200

    more test routes

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-const express = require('express'); // import express module (simplifies routing/requests, among other things)
-const app = express(); // create an instance of the express module (app is the conventional variable name used)
-const fetch = require('node-fetch'); // import node-fetch (enables the fetch API to be used server-side)
-const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (5000) on your machine
-
-//these seem to fuck up things for heroku
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const Pool = require('pg').Pool
 
@@ -15,89 +13,40 @@ const pool = new Pool({
   }
 });
 
-app.get('/', (req, res) => { // send a get request to root directory ('/' is this file (app.js))
+//TEST CONNECTION
+app.get('/', (req, res) => {
   res.send(`<h1>App is running :)</h1>`);
 })
 
-app.get('/ping', (req, res) => {
-  res.send('hello from heroku!');
-})
-
-app.get('/initialize', async (req, res) => {
-  const query = await pool.query(
-    `
-    CREATE TABLE "users" (
-      "id" SERIAL PRIMARY KEY,
-      "name" varchar(20) UNIQUE NOT NULL,
-      "premium" boolean DEFAULT false,
-      "room_keys" int DEFAULT 0
-    );
-    
-    ALTER TABLE users
-    ADD CHECK (LENGTH(name) >= 4);
-    
-    ALTER TABLE users
-    ADD CHECK (room_keys >= 0);
-    
-    CREATE TABLE "rooms" (
-      "id" SERIAL PRIMARY KEY,
-      "title" varchar(30) UNIQUE NOT NULL,
-      "description" varchar(300) NOT NULL,
-      "creator_id" int NOT NULL,
-      "next_player_id" int,
-      "turn_end" timestamp,
-      "full" boolean DEFAULT false,
-      "finished" boolean DEFAULT false
-    );
-    
-    ALTER TABLE rooms
-    ADD CHECK (LENGTH(title) >= 3);
-    
-    ALTER TABLE rooms
-    ADD CHECK (LENGTH(description) >= 3);
-    
-    CREATE TABLE "scenarios" (
-      "id" SERIAL PRIMARY KEY,
-      "number" int NOT NULL,
-      "text" varchar NOT NULL,
-      "creator_id" int NOT NULL,
-      "room_id" int NOT NULL
-    );
-    
-    ALTER TABLE scenarios
-    ADD CHECK (number >= 0);
-    
-    ALTER TABLE scenarios
-    ADD CHECK (LENGTH(text) >= 3);
-    
-    CREATE TABLE "rooms_users" (
-      "id" SERIAL PRIMARY KEY,
-      "room_id" int NOT NULL,
-      "user_id" int NOT NULL,
-      "active" boolean NOT NULL DEFAULT true,
-      "queue_number" int NOT NULL
-    );
-    
-    ALTER TABLE rooms_users
-    ADD CHECK (queue_number >= 0);
-    
-    ALTER TABLE "rooms" ADD FOREIGN KEY ("creator_id") REFERENCES "users" ("id");
-    ALTER TABLE "rooms" ADD FOREIGN KEY ("next_player_id") REFERENCES "users" ("id");
-    ALTER TABLE "scenarios" ADD FOREIGN KEY ("creator_id") REFERENCES "users" ("id");
-    ALTER TABLE "scenarios" ADD FOREIGN KEY ("room_id") REFERENCES "rooms" ("id");
-    ALTER TABLE "rooms_users" ADD FOREIGN KEY ("room_id") REFERENCES "rooms" ("id");
-    ALTER TABLE "rooms_users" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
-    `
-  );
-
-  res.send(query);
-});
-
+//GET ALL USERS
 app.get('/users', async (req, res) => {
   const query = await pool.query('SELECT * FROM users');
   res.json(query.rows);
 });
 
+//GET ALL ROOMS
+app.get('/rooms', async (req, res) => {
+  const query = await pool.query('SELECT * FROM rooms');
+});
+
+//CREATE A ROOM
+app.post('/rooms', async (req, res) => {
+  pool.query(
+    'INSERT INTO rooms (title, description, creator_id) VALUES ($1, $2, $3) RETURNING *',
+    [req.query.name, req.query.description, req.query.creatorId],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      res.status(201).send({
+        message: 'Successfully added a room to the db!',
+        person: results.rows[0]
+      });
+    }
+  )
+});
+
+//CREATE A NEW USER
 app.post('/users', async (req, res) => {
   pool.query('INSERT INTO users (name) VALUES ($1) RETURNING *', [req.query.name], (error, results) => {
     if (error) {
@@ -110,6 +59,7 @@ app.post('/users', async (req, res) => {
   })
 });
 
-app.listen(PORT, () => { // start server and listen on specified port
-  console.log(`App is running on ${PORT}`) // confirm server is running and log port to the console
+//START SERVER
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`)
 }) 
