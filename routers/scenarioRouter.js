a926543c304ce14820c1ac117e786115cd064e17
douglasdriving <douglasdriving@gmail.com>
2022-10-28 15:36:53 +0200

    progress on add scenario route

--- routers/scenarioRouter.js
+++ routers/scenarioRouter.js
@@ -1,9 +1,54 @@
 const express = require('express');
 const scenarioRouter = express.Router();
-const db = require('./dbConnect.js')
+const db = require('./dbConnect.js');
+const user = require('../fakeData/testUser');
 
-scenarioRouter.get('/', async (req, res) => { 
-  res.send('you tried to get all scenarios! lol! when would you even need that? you little shit. Dont make these requests.')
+scenarioRouter.post('/', async (req,res) => {
+
+  try {
+
+    //check: room_id provided¨
+    if(!req.query.room_id) throw new Error('No room_id provided');
+
+    //checK: scenario text provided
+    if(!req.query.text) throw new Error('No text provided');
+
+    //check: scenario text is long enough
+    if(req.query.text.length < 3) throw new Error('text must be at least 3 characters long');
+
+    //check: player has enough characters
+    const playerQuery = await db.query( //might not be necessary, since we will query this table any way and can return the player
+      'SELECT * FROM rooms_users WHERE user_id = $1',
+      [user.id]
+    )
+    if(playerQuery.rows[0].char_count < req.query.text.length) throw new Error('player does not have enough characters left');
+
+    //check: its the players turn
+    const roomQuery = await db.query(
+      'SELECT * FROM rooms WHERE id=$1',
+      [req.query.room_id]
+    );
+    if(roomQuery.rows[0].next_player_id != user.id) throw new Error('its not the logged players turn');
+
+    //check: room is not finished
+    if(roomQuery.rows[0].finished) throw new Error('the story has already been ended');
+
+    //check: deadline has not passed
+    if(roomQuery.rows[0].turn_end < new Date()){
+      //++ update who's turn it is
+      throw new Error('turn has already passed');
+    }
+    
+    //insert into scenarios
+    //set next player id (get from rooms_users)
+    //update turn end
+    //sets finished if full scenario count
+    //update player character count
+    
+  } catch (error) {
+    
+  }
+
 });
 
 module.exports = scenarioRouter;
