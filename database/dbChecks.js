bdd7c7c4bb9b1a8c3957dae341af8654dc09fc4f
douglasdriving <douglasdriving@gmail.com>
2023-02-06 13:11:24 -0300

    färdigställe post node route (buggfix)

--- database/dbChecks.js
+++ database/dbChecks.js
@@ -1,26 +1,6 @@
 //CHECKS STUFF IN THE DATABASE AND THROWS ERRORS IF THEY ARE FALSE
 const db = require('./dbConnect.js');
 
-//CONDITIONALS
-async function EmptyNodeExists(campId) {
-
-  const q = await db.query(
-    `
-    SELECT finished_at
-    FROM nodes_0
-    WHERE camp_id = $1
-    ORDER BY id DESC
-    LIMIT 1;
-    `,
-    [campId]
-  );
-
-  const { finished_at } = q.rows[0];
-
-  return (!finished_at);
-
-}
-
 //ERROR CHECKS
 async function CanAddNode(campId, userId) {
 
@@ -28,7 +8,7 @@ async function CanAddNode(campId, userId) {
     `
     SELECT
         camps.finished,
-        nodes_0.creator_id AS last_node_creator,
+        nodes_0.creator_id AS last_node_creator_id,
         nodes_0.created_at AS last_node_posted_at,
         nodes_0.finished_at AS last_node_finished_at
     FROM camps
@@ -44,6 +24,9 @@ async function CanAddNode(campId, userId) {
 
   const { finished, last_node_creator_id, last_node_posted_at, last_node_finished_at } = q.rows[0];
 
+  console.log('user id is ', userId);
+  console.log('last node added by: ', last_node_creator_id);
+
   if (finished) throw new Error('Cant add node, story is already finished');
   if (last_node_creator_id == userId) throw new Error('Cant add node, because user added the last one');
 
@@ -53,11 +36,12 @@ async function CanAddNode(campId, userId) {
   const current_time = (new Date()).getTime();
   const diff = current_time - last_node_time;
   const diffInMinutes = diff / 1000 / 60;
-  if (diffInMinutes < 20) throw new Error('Cant add node. Another player is currently writing');
+  if (diffInMinutes < 20) {
+    throw new Error('Cant add node. Another player is currently writing');
+  }
 
 }
 
 module.exports = {
   CanAddNode,
-  EmptyNodeExists,
 };
